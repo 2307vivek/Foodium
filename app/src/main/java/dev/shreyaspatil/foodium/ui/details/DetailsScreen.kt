36f411643c61cc0b9fa0f00940d2a00efe834a92
Dell <2307vivek@gmail.com>
2021-01-07 11:11:05 +0530

    setup project structure.

---
+++ app/src/main/java/dev/shreyaspatil/foodium/ui/details/DetailsScreen.kt
@@ -0,0 +1,9 @@
+package dev.shreyaspatil.foodium.ui.details
+
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun DetailsScreen() {
+    Text(text = "This is Details Screen")
+}
